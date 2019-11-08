343fc259bbd6eff9be6078bedaf7c51d7dbbbef2
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-11-08 20:58:40 +0700

    Add files via upload

---
+++ PBO1-10118040-Latihan57-Vehicle/src/pbo1/pkg10118040/latihan57/vehicle/Skaterboard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118040.latihan57.vehicle;
+
+/**
+ *
+ * @author asus
+ */
+public class Skaterboard extends Vehicle{
+    
+    private double myBoardLength;
+    
+    public Skateboard (){
+        System.out.println("Skateboard");
+    }
+
+    public double getMyBoardLength() {
+        return myBoardLength;
+    }
+
+    public void setMyBoardLength(double myBoardLength) {
+        this.myBoardLength = myBoardLength;
+    }
+}
